3f47cda0592b5a8eb75310f08be0c26cd10d6eee
darakus1420 <darakus1420@gmail.com>
2021-01-15 20:59:43 -0300

    hola

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-import Searcher from './components/Searcher'
-import PokemonView from './components/PokemonView';
+import Searcher from "./components/Searcher";
+import PokemonView from "./components/PokemonView";
 // import { readPokemon } from './modules/ApiReader'
 
 function App() {
-
   const [searched, setSearched] = useState([]);
   const [pokemons, setPokemons] = useState([]);
-
-
 
   // useEffect(() => {
   //   readPokemon()
   // }, [])
-  
 
   return (
     <div className="App">
-      <Searcher test={setSearched}/>
-      <PokemonView pokeUrl={searched.url} pokeName={searched.name}/>
+      <Searcher test={setSearched} />
+      <PokemonView pokeUrl={searched.url} pokeName={searched.name} />
     </div>
   );
 }
--- src/modules/ApiReader/index.jsx
+++ src/modules/ApiReader/index.jsx
@@ -1,33 +1,36 @@
-const BASE_API_URL = "https://pokeapi.co/api/v2/"
-const POKEMONS_TYPES_URL = BASE_API_URL + "type/"
-const POKEMONS_URL = BASE_API_URL + "pokemon/"
+const BASE_API_URL = "https://pokeapi.co/api/v2/";
+const POKEMONS_TYPES_URL = BASE_API_URL + "type/";
+const POKEMONS_URL = BASE_API_URL + "pokemon/";
 
-export const readTypes = () => {
+export const readTypes = async () => {
   return fetch(POKEMONS_TYPES_URL)
-  .then(response => {return response.json()})
-  .then(response => {
-    const typesArray = response["results"].map( (types) => {
-      return types.name
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      const typesArray = response["results"].map((types) => {
+        return types.name;
+      });
+      return typesArray;
     });
-    return typesArray;
-  })
-}
+};
 
 const capitalize = (pokemonName) => {
-  if (typeof pokemonName !== 'string') return ''
-  return pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)
-}
-
+  if (typeof pokemonName !== "string") return "";
+  return pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+};
 
 const getImgUrl = (json) => {
-  console.log(json)
-  return json["sprites"]["front_default"]
-}
+  console.log(json);
+  return json["sprites"]["front_default"];
+};
 
-export const readPokemon = (pokemon) => {
+export const readPokemon = async (pokemon) => {
   return fetch(POKEMONS_URL + pokemon)
-  .then(response => {return response.json()})
-  .then(response => {
-    return {name: capitalize(pokemon), url: getImgUrl(response)};
-  })
-}
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      return { name: capitalize(pokemon), url: getImgUrl(response) };
+    });
+};
